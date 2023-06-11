6d2c21e106ec0f2e767946ee99f804ca5800a2da
MiimeSox <kontakt@malteschumacher.de>
2023-06-11 20:24:35 +0200

    Initial Commit

---
+++ iDine2/iDine2App.swift
@@ -0,0 +1,17 @@
+//
+//  iDine2App.swift
+//  iDine2
+//
+//  Created by Malte Schumacher on 11.06.23.
+//
+
+import SwiftUI
+
+@main
+struct iDine2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
